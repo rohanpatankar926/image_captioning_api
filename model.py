338db5fa938d4bfed2c936db3a79ea728b0963b2
rohanpatankar926 <rohanpatankar95@gmail.com>
2023-02-15 02:13:48 +0530

    added files

--- model.py
+++ model.py
@@ -26,22 +26,25 @@ gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
 
 @app.route("/predict", methods=['POST'])
 def predict_step():
-    image_paths = request.form.get('image_paths').split(',')
-    images = []
-    for image_path in image_paths:
-        i_image = Image.open(image_path)
-        if i_image.mode != "RGB":
-            i_image = i_image.convert(mode="RGB")
-        images.append(i_image)
+    try:
+      image_paths = request.form.get('image_paths').split(',')
+      images = []
+      for image_path in image_paths:
+          i_image = Image.open(image_path)
+          if i_image.mode != "RGB":
+              i_image = i_image.convert(mode="RGB")
+          images.append(i_image)
 
-    pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
-    pixel_values = pixel_values.to(device)
+      pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
+      pixel_values = pixel_values.to(device)
 
-    output_ids = model.generate(pixel_values, **gen_kwargs)
-    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-    preds = [pred.strip() for pred in preds]
+      output_ids = model.generate(pixel_values, **gen_kwargs)
+      preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+      preds = [pred.strip() for pred in preds]
 
-    return jsonify({'predictions': preds})
+      return jsonify({'predictions': preds})
+    except Exception as e:
+      return jsonify({'predictions': str(e)})
   
 if __name__ == '__main__':
   app.run(debug=True,port=8000,host="0.0.0.0")
