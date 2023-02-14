5937e0505f5be73d510502c7f85aa87e51a41a71
rohanpatankar926 <rohanpatankar95@gmail.com>
2023-02-15 01:42:00 +0530

    added files

---
+++ git.sh
@@ -0,0 +1,8 @@
+# !bin/bash
+
+git init
+git add .
+git commit -m "added files"
+git remote add origin https://github.com/rohanpatankar926/image_to_text_api.git
+git checkout -b main
+git push -u origin main
