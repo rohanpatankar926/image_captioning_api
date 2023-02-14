1a598573e38b4f7508cac4f9acb439ab759106de
rohanpatankar926 <rohanpatankar95@gmail.com>
2023-02-15 01:42:32 +0530

    added files

--- git.sh
+++ git.sh
@@ -5,4 +5,4 @@ git add .
 git commit -m "added files"
 git remote add origin https://github.com/rohanpatankar926/image_to_text_api.git
 git checkout -b main
-git push -u origin main
+git push -u origin main -f 
